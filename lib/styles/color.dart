700d1b960bb8922657415c7afc66aaae49a2359b
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2023-04-28 15:45:45 +0700

    add(assets): color

---
+++ lib/styles/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryOrange = Color(0xFFFF8A50);
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+const Color primaryPurple = Color(0xFF4527A0);
+const Color primaryGray = Color(0xFFC5C1CC);
+const Color primaryGreen = Color(0xFF73FF50);
