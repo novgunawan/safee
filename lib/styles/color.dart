f79979c73885a991f7b8dab714858bfea392e861
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2023-05-02 17:00:20 +0700

    finish(ui): onboarding screen

--- lib/styles/color.dart
+++ lib/styles/color.dart
@@ -7,5 +7,6 @@ const Color primaryPurple = Color(0xFF4527A0);
 const Color primaryGray = Color(0xFFC5C1CC);
 const Color primaryGreen = Color(0xFF73FF50);
 const Color secondaryGray = Color(0xFF707070);
+const Color lightGray = Color(0xFFE9E9E9);
 const Color lightGradient = Color(0xFF6850FF);
 const Color heavyGradient = Color(0xFF1700A3);
