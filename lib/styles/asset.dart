f2face45f04496e384ab4dbd6cb856a6b278b4e8
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2023-05-02 14:41:09 +0700

    add(assets): - colors - text style - configure loading widget

---
+++ lib/styles/asset.dart
@@ -0,0 +1,7 @@
+const String baseImage = 'assets/images';
+
+// IMAGES
+
+String ONBOARDING_0 = '$baseImage/onboarding-0.png';
+String WALLET = '$baseImage/wallet.png';
+// ICONS
--- lib/styles/color.dart
+++ lib/styles/color.dart
@@ -6,3 +6,6 @@ const Color white = Color(0xFFFFFFFF);
 const Color primaryPurple = Color(0xFF4527A0);
 const Color primaryGray = Color(0xFFC5C1CC);
 const Color primaryGreen = Color(0xFF73FF50);
+const Color secondaryGray = Color(0xFF707070);
+const Color lightGradient = Color(0xFF6850FF);
+const Color heavyGradient = Color(0xFF1700A3);
