1bd43cc65cd8f6617a2629e6fcbae9c7d83078de
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2023-05-19 15:13:43 +0700

    handle: network

--- lib/base/api/api_response.g.dart
+++ lib/base/api/api_response.g.dart
@@ -15,3 +15,13 @@ ApiResponse<T> _$ApiResponseFromJson<T>(
       message: json['message'] as String,
       data: fromJsonT(json['data']),
     );
+
+Map<String, dynamic> _$ApiResponseToJson<T>(
+  ApiResponse<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'data': toJsonT(instance.data),
+    };
